276fbc651dc902c142f7ee1fc3bd0c51e8192510
eddiex666 <eddiex@fx-2.org>
2019-03-09 17:24:36 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,124 @@
+# SSBv1 Support for Homey
+
+This Project allows Control of SSBv1 devices via [Homey](https://www.athom.com/en/) over TCP/IP
+
+## Requirements
+
+1) [openWebif](https://github.com/E2OpenPlugins/e2openplugin-OpenWebif) needs to be available on the SSBv1 device
+2) Setup of SSBv1 App via [Homey](https://www.athom.com/en/) required.
+   Start Homey App, open the More section, select Apps Menu, open SSBv1 entry and Click on "Configure app"
+
+## Support Overview
+
+### when / trigger flowcards
+
+- [ ] polling required, therefor currently not decided. If you like to put in arguments, visit [Athom Community](https://community.athom.com/t/SSBv1-dvb-boxes-support/9427)
+
+### and / condition flowcards
+
+- [ ] Power is ON
+- [ ] Power is OFF
+- [ ] Radio mode is used
+- [ ] TV mode is used
+- [ ] ...to be continued if you have ideas, visit [Athom Community](https://community.athom.com/t/SSBv1-dvb-boxes-support/9427) and let me know
+
+### then / action flowcards
+
+- [x] [deep standby mode](#deep-standby-mode)
+- [x] reboot SSBv1 software
+- [x] reboot receiver
+- [x] send command as ID (see below for Info)
+- [x] [send message](#messages)
+- [x] standby mode
+- [x] standby wake
+- [x] Volume mute
+- [x] Volume set (0% - 100%)
+- [x] Volume unmute
+- [ ] ...to be continued if you have ideas, visit [Athom Community](https://community.athom.com/t/SSBv1-dvb-boxes-support/9427) and let me know
+
+### Supported Languages
+
+- [ ] dutch [Contribute here](https://community.athom.com/t/SSBv1-dvb-boxes-support/9427) or file a pull request on GitHub
+- [x] english
+- [x] german
+
+## deep standby mode
+
+**WARNING:**
+After this flowcard is executed you can't control the box over the Network Interface anymore!
+
+## Messages
+
+During the Input you need to make sure you follow these Format:
+    Type|Timeout|Message
+inbetween the different Sections no Spaces are required.
+Here are the different Supported Variables:
+Type:
+  - 0 = Yes / No (currently only Display, no action yet implemented)
+  - 1 = Info Message
+  - 2 = Plain Message
+  - 3 = Attention Message
+Timeout:
+  - set it to 0 to be endless displayed or Provide Time in seconds
+Message:
+  - Type your Text here, to make a new line use \n in the Text without a Space afterwards.
+
+## For Supported Devices check the following
+
+Due to limited access to all SSBv1 enabled devices, i was only able to test the functions at an Dreambox 800HDse and a few less on an VU+ duo2, if any Issues are observed, please file an Issue on [GitHub](https://github.com/carp3-noctem/eu.carp3-noctem.SSBv1/issues) or Post to the [Athom Community Forum Topic](https://community.athom.com/t/SSBv1-dvb-boxes-support/9427).
+
+1. Check Google Spreadsheet: [Google Spreadsheet](https://docs.google.com/spreadsheets/d/1DlcXXRLvs-AKHAxlF2KolwLmICR3OC4liS-9Cn0K48c/edit?usp=sharing)
+   Green = Fully Supported  
+   empty Green = Not Programmed in this Device
+2. Pictures of the Boxes can be found here: [OpenWebif Box Pictures](https://github.com/E2OpenPlugins/e2openplugin-OpenWebif/tree/master/plugin/public/images/boxes)
+3. Pictures of the Remotes can be found here: [OpenWebif Remote Pictures](https://github.com/E2OpenPlugins/e2openplugin-OpenWebif/tree/master/plugin/public/images/remotes)
+
+## Changelog
+
+## Version 0.1.2
+
+- App is now in Beta
+- Text Alpha to Beta changed
+
+## Version 0.1.0
+
+- Better Log for Alpha Version (all actions will be loged)
+
+## Version 0.0.9
+
+- APPSTORE.md implemented, due to false handling of Markdown at Athom Appstore
+- console.log Output for wrong calls implemented
+
+## Version 0.0.8
+
+- German Language Support added
+
+## Version 0.0.7
+
+- Update to enable Homey Firmware 1.5.13 for testing
+
+## Version 0.0.6
+
+- reorder of then / action flowcards
+- redo README.md for better overview
+
+## Version 0.0.5
+
+- Added Message Function (special Format required for Text Input!)
+- Corrected Type at send Box to Standby Function
+
+## Version 0.0.4
+
+- Updated Web Adresses to Human readable short URL's
+
+## Version 0.0.3
+
+- Added Send CommandID via then / Action Flowcard for Supported ID's visit [http://SSBv1.carpe3-noctem.eu](http://SSBv1.carpe3-noctem.eu)
+
+## Version 0.0.2
+
+- Changed From Athom Forum to Community Topic
+
+## Version 0.0.1
+
+- Initial Release
---
+++ app.js
@@ -0,0 +1,235 @@
+'use strict';
+const Homey = require('homey');
+var request = require('request');
+
+// Build SSBv1 Host from Config (make variables single for later use as well)
+const SSBv1_IP = Homey.ManagerSettings.get('SSBv1_ip');
+const SSBv1_Port = Homey.ManagerSettings.get('SSBv1_port');
+var SSBv1Host = SSBv1_IP + ':' + SSBv1_Port;
+
+//Homey Requirement
+class SSBv1 extends Homey.App {
+	onInit() {
+		this.log(Homey.__("appLogStart"));
+	}
+}
+
+
+// Start Volume
+// Function Volume Control
+function VolumeControl(Volume) {
+	//var volumeControl_call = ('http://' + SSBv1Host + '/web/vol?set=' + Volume);
+	var volumeControl_call = ('http://' + SSBv1Host + '/UIC?cmd=%3Cname%3ESetVolume%3C/name%3E%3Cp%20type=%22dec%22%20name=%22volume%22%20val=%22' + Volume + '%22/%3E');
+	// http://10.0.0.147:55001/UIC?cmd=%3Cname%3ESetVolume%3C/name%3E%3Cp%20type=%22dec%22%20name=%22volume%22%20val=%2220%22/%3E
+	var options = {
+		url: volumeControl_call,
+		headers: { SSBv1Host }
+	};
+	function callback(error, response, body) {
+		if (!error && response.statusCode == 200){
+			console.log('Following Call was made successfully(Volume): \n'+volumeControl_call);
+		}
+		else {
+			console.log('Following Call was made successfully(Volume): \n'+volumeControl_call);
+			//console.log('Following Error occured when Fuction VolumeControl was called: \n Call used:'+volumeControl_call+'\nError:' +!error+ '\nResponse Status Code: '+response.statusCode+ '\nBody: \n'+body+'\n');
+			}
+	}
+	request(options, callback);
+}
+// Function Mute Control
+function VolumeControlMute (e2ismuted) {
+	var volumeMute_call = ('http://' + SSBv1Host + '/web/vol');
+	var options = {
+		url: volumeMute_call,
+		headers: { SSBv1Host }
+	  };
+	  function callback(error, response, body) {
+		if (!error && response.statusCode == 200) {
+		  if (e2ismuted === 'mute' && body.indexOf('False') !== -1) {
+				VolumeControl('mute')
+				console.log('Following Call was made successfully(mute): \n'+volumeMute_call);
+		  }
+		  if (e2ismuted === 'unmute' && body.indexOf('True') !== -1) {
+			VolumeControl('down');
+			VolumeControl('up');
+			console.log('Following Call was made successfully(unmute): \n'+volumeMute_call);
+		  }
+		  else {
+				console.log('Following Error occured when Fuction VolumeControlMute was called: \nCall used: '+volumeMute_call+' \nError:' +!error+ '\nResponse Status Code: '+response.statusCode+ '\nBody: \n'+body+'\n');
+			}
+		}
+	  }
+	  request(options, callback);
+}
+
+
+// then / action Flowcards Volume
+//Set Mute Flowcard
+let setMuteAction = new Homey.FlowCardAction('vol_mute');
+setMuteAction
+  .register()
+  .registerRunListener(() => {
+	VolumeControlMute('mute');
+    return true;
+});
+//Set Unmute Flowcard
+let setUnmuteAction = new Homey.FlowCardAction('vol_unmute');
+setUnmuteAction
+  .register()
+  .registerRunListener(() => {
+	VolumeControlMute('unmute');
+    return true;
+});
+//Set Volume Flowcard
+let setVolumeAction = new Homey.FlowCardAction('vol_set');
+setVolumeAction
+  .register()
+  .registerRunListener(( args) => {
+	  var Volume = args.volume;
+	VolumeControl(Volume);
+    return true;
+});
+// End Volume
+
+
+// Start Power Control
+// Function Power On / Off decision
+function checkPowerState(powerstate) {
+	var checkPower_call = ('http://' + SSBv1Host + '/web/powerstate');
+	var options = {
+		url: checkPower_call,
+		headers: { SSBv1Host }
+	};
+	function callback(error, response, body) {
+		if (!error && response.statusCode == 200) {
+			if (powerstate === 'ON' && body.indexOf('true') !== -1) {
+				sendCommandID('powerstate?newstate=0');
+				console.log('Following Call was made successfully(Power ON = true): \n'+checkPower_call);
+			}
+			if (powerstate === 'OFF' && body.indexOf('false') !== -1) {
+				sendCommandID('powerstate?newstate=0');
+				console.log('Following Call was made successfully(Power ON = false): \n'+checkPower_call);
+			}
+		}
+		else {
+			console.log('Following Error occured when Fuction checkPowerState was called: \n Call used: '+checkPower_call+'\nError:' +!error+ '\nResponse Status Code: '+response.statusCode+ '\nBody: \n'+body+'\n');
+		}
+	}
+	request(options, callback);
+}
+
+
+// then / action Flowcards Power
+//Power On Flowcard
+let powerstateONAction = new Homey.FlowCardAction('powerstate_on');
+powerstateONAction
+	.register()
+	.registerRunListener(() => {
+		checkPowerState('ON');
+		return true;
+	});
+//Power Off Flowcard
+let powerstateOFFAction = new Homey.FlowCardAction('powerstate_off');
+powerstateOFFAction
+	.register()
+	.registerRunListener(() => {
+		checkPowerState('OFF');
+		return true;
+	});
+//Restart SSBv1 Software Flowcard
+let powerstateRestartE2Action = new Homey.FlowCardAction('powerstate_restart_e2');
+powerstateRestartE2Action
+	.register()
+	.registerRunListener(() => {
+		sendCommandID('powerstate?newstate=3');
+		return true;
+	});
+//Restart Receiver Flowcard
+let powerstateRebootBoxAction = new Homey.FlowCardAction('powerstate_rebootBox');
+powerstateRebootBoxAction
+	.register()
+	.registerRunListener(() => {
+		sendCommandID('powerstate?newstate=2');
+		return true;
+	});
+//Send Receiver to Deep Standby Flowcard
+let powerstateSendToDeepStandbyAction = new Homey.FlowCardAction('powerstate_deepStandy');
+powerstateSendToDeepStandbyAction
+	.register()
+	.registerRunListener(() => {
+		sendCommandID('powerstate?newstate=1');
+		return true;
+	});
+// End Power Control
+
+
+// Command ID Send Start
+// Function Command ID
+function sendCommandID(commandID) {
+	var command_call = ('http://' + SSBv1Host + '/web/' + commandID);
+	var options = {
+		url: command_call,
+		headers: { SSBv1Host }
+	};
+	function callback(error, response, body) {
+		if (!error && response.statusCode == 200) {
+			console.log('Following Call was made successfully: \n'+command_call);
+			}
+	else {
+		console.log('Following Error occured when Fuction sendCommandID was called: \n Call used: '+command_call+'\nError:' +!error+ '\nResponse Status Code: '+response.statusCode+ '\nBody: \n'+body+'\n');
+		}
+	}
+	request(options, callback);
+}
+// then / action Flowcards Command ID
+//Send Command ID Flowcard
+let sendCommandAction = new Homey.FlowCardAction('command_send');
+sendCommandAction
+  .register()
+  .registerRunListener((args) => {
+	  var commandID = 'remotecontrol?command=' + args.command;
+	  sendCommandID(commandID);
+    return true;
+});
+// Command ID Send End
+
+// Send Message Start
+// Function send Message
+function sendMessage(msg_txt, msg_type, msg_timeout) {
+	var msg_call = ("http://"+ SSBv1Host + "/web/message?text=" + msg_txt + "&type=" + msg_type + "&timeout=" + msg_timeout);
+	var options={
+		url: msg_call,
+		headers: { SSBv1Host }
+	};
+	function callback(error, response, body){
+		if (!error && response.statusCode == 200){
+			console.log('Following Call was made successfully: \n'+msg_call);
+		}
+		else {
+			console.log('Following Error occured when Fuction sendMessage was called: \n Call used: '+ msg_code + '\nError:' +!error+ '\nResponse Status Code: '+response.statusCode+ '\nBody: \n'+body+'\n');
+		}
+	}
+	request(options, callback);
+}
+// then / action Flowcards send Message
+let sendMessageAction = new Homey.FlowCardAction('message_send');
+sendMessageAction
+  .register()
+  .registerRunListener((args) => {
+		var message_complete = args.msg_text_full;
+		var message_split = message_complete.split("|");
+		var msg_type = message_split[0];
+		var timeout = message_split[1];
+		var msg_txt = message_split[2];
+	  if (timeout == 0) {
+			var msg_timeout = "";
+		} else {
+			var msg_timeout = timeout;
+		}
+	  sendMessage(msg_txt,msg_type, msg_timeout);
+    return true;
+});
+
+// Send Message End
+
+module.exports = SSBv1;
